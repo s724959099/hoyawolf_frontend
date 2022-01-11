633d92554d4b0ec84269ee8ff002c654f5ac1471
Hazel Wu <tinazx056@gmail.com>
2022-01-12 00:27:46 +0800

    feat(setup): install axios, qs, vue-axios, mkcert local https, proxy cors

--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import vuetify from "./plugins/vuetify";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import vuetify from './plugins/vuetify'
 
-Vue.config.productionTip = false;
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+Vue.use(VueAxios, axios)
+
+import Qs from 'qs'
+Vue.prototype.Qs = Qs
+
+Vue.config.productionTip = false
 
 new Vue({
   router,
   vuetify,
   render: (h) => h(App),
-}).$mount("#app");
+}).$mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,26 @@
+const fs = require('fs')
+
 module.exports = {
-  transpileDependencies: ["vuetify"],
-};
+  transpileDependencies: ['vuetify'],
+  productionSourceMap: false,
+  devServer: {
+    https: {
+      key: fs.readFileSync(`${__dirname}/src/assets/https/localhost-key.pem`),
+      cert: fs.readFileSync(`${__dirname}/src/assets/https/localhost.pem`),
+    },
+    disableHostCheck: true,
+    port: 9527,
+    open: true,
+    hot: true,
+    proxy: {
+      '/line': {
+        target: process.env.VUE_APP_LINE_API,
+        changeOrigin: true,
+        secure: false,
+        pathRewrite: {
+          '/line': '',
+        },
+      },
+    },
+  },
+}
