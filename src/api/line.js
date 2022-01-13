d749522a51ba2aef7eacbde3db96d5ef870bebbc
Hazel Wu <tinazx056@gmail.com>
2022-01-14 02:40:04 +0800

    feat(api): setup user api

--- src/api/line.js
+++ src/api/line.js
@@ -1,18 +1,6 @@
 import { lineRequest, notifyRequest } from '@/utils/line-request'
 import request from '@/utils/request'
 
-// [Line Notify 服務] Notify 拿 code 換 Access Token
-export function getNotifyAccessTokenAPI(data) {
-  return notifyRequest({
-    url: '/oauth2/token',
-    method: 'post',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    data,
-  })
-}
-
 // [Line 服務] 取得 Access Token，以取得使用者基本資訊
 export function getLineAccessTokenAPI(data) {
   return lineRequest({
@@ -37,15 +25,27 @@ export function getNotifyAuthorizationAPI(data) {
   })
 }
 
+// [Line Notify 服務] [Step2] 拿 code 換 Access Token
+export function getNotifyAccessTokenAPI(data) {
+  return notifyRequest({
+    url: '/oauth2/token',
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    data,
+  })
+}
+
 // [後端] [Step1] 請求 Notify 取得 User Authorization
 export function lineRedirectAPI(url) {
   return `${process.env.VUE_APP_WOLF_API}/api/line/redirect?redirect_uri=${url}&state=913458sadg123`
 }
 
 // [後端] [Step2] 拿 code 取得 User Notify Access Token
-export function getUserNotifyAccessTokenAPI(code, url) {
+export function getUserNotifyAccessTokenAPI(code) {
   return request({
-    url: `/api/line/line_notify_callback?code=${code}&site_url=${url}&state=b1234jk93851234`,
+    url: `/api/line/line_notify_callback?code=${code}`,
     method: 'get',
   })
 }
--- src/api/user.js
+++ src/api/user.js
@@ -1,29 +1,26 @@
 import request from '@/utils/request'
 
 // 註冊 Opensea 地板價通知
-export function registerOpenseaNotify(data) {
+export function registerOpenseaNotifyAPI(data) {
   return request({
     url: `/api/line/opensea/register`,
     method: 'post',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
     data,
   })
 }
 
-// 取得該 user 所有的 opensea 推播通知
-export function getUserInfo({ user_id }) {
+// 取得該 User 所有的 opensea 推播通知
+export function getUserInfoAPI(user_id) {
   return request({
-    url: `/api/user/info?${user_id}`,
+    url: `/api/user/info/${user_id}`,
     method: 'get',
   })
 }
 
-// 取得該 user 所有的 opensea 推播通知
-export function getUserAllOsNotifyAPI({ token_id }) {
+// 刪除 User Token
+export function deleteUserAPI(token_id) {
   return request({
-    url: `/api/user/token?${token_id}`,
+    url: `/api/user/token/${token_id}`,
     method: 'delete',
   })
 }
