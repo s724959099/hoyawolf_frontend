528163a0aad12e9b131f20d44b16d80d4498112e
Hazel Wu <tinazx056@gmail.com>
2022-01-17 18:56:38 +0800

    feat(title): setup page title

--- src/permission.js
+++ src/permission.js
@@ -2,6 +2,7 @@ import router from './router'
 // import store from './store'
 
 router.beforeEach(async (to, from, next) => {
+  document.title = 'HoyaWolf!'
   next()
   // if (store.state.isLogin) {
   //   next()
